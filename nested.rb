91383fff16bbcf36edf7c25f0e47f2198b48f389
Nick Bazaldua <bazalnick@gmail.com>
2020-08-03 01:19:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,6 +104,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+      programmer_hash[alan_kay]
       alans_new_info = "GUI"
      
      
@@ -125,5 +126,4 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[alan_kay]
 end
